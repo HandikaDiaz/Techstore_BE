f302dce3c96e92def28878f4eadbfeab3d12e466
Alexa <handikaalexandriadiaz@gmail.com>
2026-01-20 07:38:23 +0700

    Fixing Deployment

--- index.ts
+++ index.ts
@@ -29,7 +29,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ["https://techstore-fe.vercel.app/", "http://localhost:5173"],
+    origin: [
+        "http://localhost:5173",
+        process.env.FRONTEND_URL as string,
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
